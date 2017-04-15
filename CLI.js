2edf2e368fdb484a28c82774c4e6e25c4da947d7
narst20 <narst20@gmail.com>
2017-04-15 10:58:56 -0400

    Main file to run with node

---
+++ CLI.js
@@ -0,0 +1,17 @@
+//Psuedo Code
+	//1. Read if third entry in commandline is user or admin
+	//2. If it is user,we want to run the two function listed to pull the data
+	//3. If it is admin, we want to open the log.txt to see all the searches that were run
+	//4. If it is neither, we want them to try again.
+
+	if (process.argv[2]=="user") {
+
+	}
+	else if (process.argv[2]=="admin") {
+
+	}
+	else {
+		console.log("Error! Please write user or admin");
+	}
+
+
